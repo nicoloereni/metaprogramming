5658dd59b925c3f05f70d9cb34eaf26d2279b4f7
Nicolo Ereni <nicolo.ereni@nozominetworks.com>
2022-09-02 10:45:10 +0200

    fix and extend tests

--- story_of_bob/test/entity_test.rb
+++ story_of_bob/test/entity_test.rb
@@ -1,15 +1,18 @@
 require "test/unit"
 require_relative '../lib/entity'
+require 'mocha/setup'
+
 class EntityTest < Test::Unit::TestCase
   setup do
+    SecureRandom.stubs(:uuid).returns('an_id')
     @entity = Entity.new
   end
 
   def test_get
-    assert_equal('SELECT a_column FROM a_table WHERE id=an_id', @entity.get('a_column'))
+    assert_equal('SELECT a_column FROM Entity WHERE id=an_id', @entity.get('a_column'))
   end
 
   def test_set
-    assert_equal("UPDATE a_table SET a_column='a_val' WHERE id=an_id", @entity.set('a_column', 'a_val'))
+    assert_equal("UPDATE Entity SET a_column='a_val' WHERE id=an_id", @entity.set('a_column', 'a_val'))
   end
 end
--- story_of_bob/test/movie_test.rb
+++ story_of_bob/test/movie_test.rb
@@ -1,10 +1,17 @@
 require 'test/unit'
 require_relative '../lib/movie'
+require 'mocha/setup'
 
 class MovieTest < Test::Unit::TestCase
+  setup do
+    SecureRandom.stubs(:uuid).returns('an_id')
+  end
+
   def test_movie_creation
     movie = Movie.new
     assert_equal('a title', movie.title = "a title")
     assert_equal('a director', movie.director = "a director")
+    assert_equal('SELECT title FROM Movie WHERE id=an_id', movie.get('title'))
+    assert_equal("UPDATE Movie SET title='pirippi' WHERE id=an_id", movie.set('title', 'pirippi'))
   end
 end
