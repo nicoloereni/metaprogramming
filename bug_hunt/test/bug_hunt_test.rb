8fbcb714ef47744bcbd373de480678b23160f404
Nicolo Ereni <nicolo.ereni@nozominetworks.com>
2022-10-05 09:26:46 +0200

    * bug hunt

--- bug_hunt/test/bug_hunt_test.rb
+++ bug_hunt/test/bug_hunt_test.rb
@@ -4,8 +4,8 @@ require_relative '../lib/bug_hunt'
 class BugHuntTest < Test::Unit::TestCase
   def test_roulette
     roulette = BugHunt.new
-    p roulette.pippo
-    p roulette.paperino
+    assert_true roulette.pippo.include? "Pippo got a "
+    assert_true roulette.paperino.include? "Paperino got a "
     assert_raise(NoMethodError) { roulette.dwede }
   end
 end
